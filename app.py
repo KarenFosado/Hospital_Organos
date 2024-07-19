b6ac311bbe9ccca39c9c70a68c784705481da330
Alyn.Fosado <210764@utxicotepec.edu.mx>
2024-07-19 13:44:47 -0600

    Creacion de BD Local

---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routes.user import user
+from routes.person import person
+from routes.rol import rol
+from routes.userrol import userrol
+
+app = FastAPI()  # variable app
+app.include_router(person)  # Luego agrega las rutas
+app.include_router(user)
+app.include_router(rol)
+app.include_router(userrol)
+
+print("Bienvenido al servidor uvicorn")
---
+++ config/db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker 
+
+SQLALCHEMY_DATABASE_URL = "mysql://avnadmin:AVNS_uYK5Oroy0wcRrJYtFOc@mysql-3299278f-utxicotepec-623d.h.aivencloud.com:11209/defaultdb?ssl-mode=REQUIRED"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base= declarative_base()
+
+
+
+
