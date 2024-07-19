c23200c9065d40a7a8c751d4f77d8a310b261a44
Alyn.Fosado <210764@utxicotepec.edu.mx>
2024-07-19 13:49:17 -0600

    BD LOCAL

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker 
 
-SQLALCHEMY_DATABASE_URL = "mysql://avnadmin:AVNS_uYK5Oroy0wcRrJYtFOc@mysql-3299278f-utxicotepec-623d.h.aivencloud.com:11209/defaultdb?ssl-mode=REQUIRED"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base= declarative_base()
